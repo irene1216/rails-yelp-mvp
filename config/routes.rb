bdc4f2db9b98d74b742a4c63bc5c745619e25881
Irene Chan <irene.chan.16@outlook.com>
2019-07-24 16:47:50 +0800

    restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'restaurant/:restaurant_id/reviews/new', to: 'reviews#new'
+  post 'restaurant/:restaurant_id/reviews', to: 'reviews#create'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show'
+  get 'restaurants', to: 'restaurants#index'
+  post 'restaurants', to: 'restaurants#create'
+    resources :restaurants do
+        resources :reviews, only: [ :new, :create ]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+    #                                       Controller#Action
+    #    restaurant_reviews POST /restaurant/:restaurant_id/reviews(.:format)         reviews#create
+    # new_restaurant_review GET  /restaurant/:restaurant_id/reviews/new(.:format)     reviews#new
+    #      restaurant_index GET  /restaurant(.:format)                                restaurant#index
+    #                       POST /restaurant(.:format)                                restaurant#create
+    #        new_restaurant GET  /restaurant/new(.:format)                            restaurant#new
+    #            restaurant GET  /restaurant/:id(.:format)                            restaurant#show
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+Restaurant.destroy_all if Rails.env.development?
+
+CATEGORY = %w(chinese italian japanese french belgian)
+5.times do
+  Restaurant.create!(name: Faker::Kpop.i_groups,
+                    phone_number: Faker::PhoneNumber.phone_number,
+                    address: Faker::Address.street_name,
+                    category: CATEGORY.sample)
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +14,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# rails generate model Restaurant name:string address:string phone_number:string category:string
+# rails generate model Review content:text rating:integer Restaurant:reference
